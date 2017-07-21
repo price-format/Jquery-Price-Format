4a101673392e2e069a7bf392ffa1a9bf0491cfd4
Fernando Moreira <nandomoreira.me@gmail.com>
2017-07-20 22:22:34 -0300

    gulpfile.js using es6 const

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,11 @@
-// Variables
-var gulp        = require('gulp');
-var minify      = require('gulp-minify');
-var ghPages     = require('gulp-gh-pages');
-var htmlmin     = require('gulp-htmlmin');
-var cssnano     = require('gulp-cssnano');
-var gulpSize    = require('gulp-size');
-var browserSync = require('browser-sync').create();
-var reload      = browserSync.reload;
+const gulp = require('gulp')
+const minify = require('gulp-minify')
+const ghPages = require('gulp-gh-pages')
+const htmlmin = require('gulp-htmlmin')
+const cssnano = require('gulp-cssnano')
+const gulpSize = require('gulp-size')
+const browserSync = require('browser-sync').create()
+const reload = browserSync.reload
 
 // Compress javascripts
 gulp.task('javascripts', () => gulp.src([
@@ -21,16 +20,16 @@ gulp.task('javascripts', () => gulp.src([
   }))
   .pipe(gulpSize({ title: 'Compress javascripts...', gzip: false, showFiles: true }))
   .pipe(gulp.dest('.dist/javascripts'))
-  .pipe(gulp.dest('.')));
+  .pipe(gulp.dest('.')))
 
 gulp.task('docs:css', () => gulp.src('./docs/stylesheets/*.css')
   .pipe(cssnano())
   .pipe(gulpSize({ title: 'Minify CSS doc files...', gzip: false, showFiles: true }))
-  .pipe(gulp.dest('.dist/stylesheets/')));
+  .pipe(gulp.dest('.dist/stylesheets/')))
 
 gulp.task('docs:images', () => gulp.src('./docs/images/*')
   .pipe(gulpSize({ title: 'Minify CSS doc files...', gzip: false, showFiles: true }))
-  .pipe(gulp.dest('.dist/images/')));
+  .pipe(gulp.dest('.dist/images/')))
 
 gulp.task('docs:js', () => gulp.src('./docs/javascripts/demo.js')
   .pipe(minify({
@@ -39,30 +38,30 @@ gulp.task('docs:js', () => gulp.src('./docs/javascripts/demo.js')
     }
   }))
   .pipe(gulpSize({ title: 'Minify javascripts demo file...', gzip: false, showFiles: true }))
-  .pipe(gulp.dest('.dist/javascripts/')));
+  .pipe(gulp.dest('.dist/javascripts/')))
 
 gulp.task('docs:html', () => gulp.src('./docs/**/*.html')
   .pipe(htmlmin({ collapseWhitespace: true }))
   .pipe(gulpSize({ title: 'Minify HTML doc files...', gzip: false, showFiles: true }))
-  .pipe(gulp.dest('.dist/')));
+  .pipe(gulp.dest('.dist/')))
 
-gulp.task('build', ['javascripts', 'docs:html', 'docs:css', 'docs:images', 'docs:js']);
+gulp.task('build', ['javascripts', 'docs:html', 'docs:css', 'docs:images', 'docs:js'])
 
 gulp.task('watch', ['build'], () => {
-  gulp.watch(['*.priceformat.js'], ['javascripts']);
-  gulp.watch(['docs/**/*'], ['build']);
-  gulp.watch(['docs/*.html', 'docs/**/*.css', 'docs/**/*.js']).on('change', reload);
-});
+  gulp.watch(['*.priceformat.js'], ['javascripts'])
+  gulp.watch(['docs/**/*'], ['build'])
+  gulp.watch(['docs/*.html', 'docs/**/*.css', 'docs/**/*.js']).on('change', reload)
+})
 
 gulp.task('server', ['watch'], () => {
   browserSync.init({
     server: {
       baseDir: './.dist'
     }
-  });
-});
+  })
+})
 
 gulp.task('deploy', ['build'], () => gulp.src('./.dist/**/*')
-  .pipe(ghPages()));
+  .pipe(ghPages()))
 
-gulp.task('default', ['server']);
+gulp.task('default', ['server'])
